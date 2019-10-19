9f8604824a62502eb5935f4580927f90b83b8b27
Lukas <korciciak.lukas@gmail.com>
2019-10-19 20:22:47 +0200

    graphic design

--- app/src/main/java/com/example/projectiletrajectory/MainActivity.java
+++ app/src/main/java/com/example/projectiletrajectory/MainActivity.java
@@ -18,14 +18,6 @@ import com.google.gson.JsonElement;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.util.ArrayList;
 import retrofit.Callback;
 import retrofit.RestAdapter;
